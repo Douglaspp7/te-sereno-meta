2333da6b29aa0d0eeff70007910f4f90b7da2437
Douglas <douglaspp7@gmail.com>
2026-06-05 13:41:56 -0300

    chore: add user video links for first two days via migration

---
+++ supabase/migrations/20240101000001_update_videos.sql
@@ -0,0 +1,7 @@
+UPDATE public.exercises 
+SET video_url = 'https://youtu.be/YFAuNBwvugY' 
+WHERE id = '40000000-0000-0000-0000-000000004001';
+
+UPDATE public.exercises 
+SET video_url = 'https://youtu.be/21C7hlYOnwE' 
+WHERE id = '40000000-0000-0000-0000-000000004002';
