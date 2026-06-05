16a116e742c4a7e55a18f4cefa1f29fdce58f2f1
Douglas <douglaspp7@gmail.com>
2026-06-05 20:17:59 -0300

    fix: use standard URL import for react-pdf worker

--- src/components/PdfViewer.tsx
+++ src/components/PdfViewer.tsx
@@ -2,7 +2,10 @@ import { Document, Page, pdfjs } from 'react-pdf';
 import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
 import 'react-pdf/dist/esm/Page/TextLayer.css';
 
-pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+  'pdfjs-dist/build/pdf.worker.min.mjs',
+  import.meta.url,
+).toString();
 
 interface Props {
   file: string;
