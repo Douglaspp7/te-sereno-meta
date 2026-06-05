fcb8c1644ffcca5285ba463d6e95dfb0160f8fda
Douglas <douglaspp7@gmail.com>
2026-06-05 19:44:16 -0300

    fix: externalize canvas in vite config for react-pdf

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,11 @@ export default defineConfig({
     server: { entry: "server" },
   },
   vite: {
+    build: {
+      rollupOptions: {
+        external: ['canvas'],
+      },
+    },
     plugins: [
       VitePWA({
         registerType: "autoUpdate",
