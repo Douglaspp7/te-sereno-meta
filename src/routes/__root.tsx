5cbbce5d43cec86ef8d995be60f9ad8da64b8bd8
Douglas <douglaspp7@gmail.com>
2026-06-07 16:16:33 -0300

    fix: correct case sensitivity in PWAInstallPrompt import

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -11,7 +11,7 @@ import { useEffect, type ReactNode } from "react";
 
 import appCss from "../styles.css?url";
 import { reportLovableError } from "../lib/lovable-error-reporting";
-import { PWAInstallPrompt } from "../components/PwaInstallPrompt";
+import { PWAInstallPrompt } from "../components/PWAInstallPrompt";
 
 function NotFoundComponent() {
   return (
